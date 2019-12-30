d3380ae6c0500ae9973eed71c76c0a0fbc32e6ba
Fernando Becker <fernandobgentile@gmail.com>
2019-12-30 11:43:03 -0300

    chessException and chessPosition

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -41,7 +41,7 @@ public class Board {
 	}
 	
 	public boolean thereIsAPiece(Position position) {
-		if(!positionExists(position)) {
+		if(!postionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 		return piece(position)!=null;
